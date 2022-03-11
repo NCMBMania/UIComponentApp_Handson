731a99122e538ee928fa3e95aebd917f45a7c876
Atsushi <atsushi@moongift.jp>
2022-03-11 16:52:48 +0900

    init

--- www/js/app.js
+++ www/js/app.js
@@ -1,30 +1,13 @@
 const $ = Dom7;
-const app = new Framework7({
-  name: 'My App', // App name
-  theme: 'auto', // Automatic theme detection
-  el: '#app', // App root element
 
-
-  // App store
-  store: store,
-  // App routes
-  routes: routes,
-});
-
-// Login Screen Demo
-$('#my-login-screen .login-button').on('click', function () {
-  var username = $('#my-login-screen [name="username"]').val();
-  var password = $('#my-login-screen [name="password"]').val();
-
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
-
-  // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br/>Password: ' + password);
-});
-
-// NCMBの初期化用
 (async () => {
   const config = await (await fetch('./js/config.json')).json();
   window.ncmb = new NCMB(config.applicationKey, config.clientKey);
+  mapboxgl.accessToken = config.mapboxToken;
+  window.app = new Framework7({
+    el: '#app', // App root element
+    name: 'framework7-core-tab-view', // App name
+    theme: 'auto', // Automatic theme detection
+    routes: routes,
+  });
 })();
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,79 +1,51 @@
+
 const routes = [
   {
     path: '/',
     url: './index.html',
   },
   {
-    path: '/about/',
-    url: './pages/about.html',
+    path: '/login/',
+    componentUrl: './pages/auth.html',
+    beforeEnter: function ({ resolve, reject, router }) {
+      const user = ncmb.User.getCurrentUser();
+      if (user) {
+        reject();
+        router.navigate('/');
+      } else {
+        resolve();
+      }
+    }
   },
   {
-    path: '/form/',
-    url: './pages/form.html',
-  },
-  {
-    path: '/catalog/',
-    componentUrl: './pages/catalog.html',
-  },
-  {
-    path: '/product/:id/',
-    componentUrl: './pages/product.html',
-  },
-  {
-    path: '/settings/',
-    url: './pages/settings.html',
-  },
-
-  {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    componentUrl: './pages/dynamic-route.html',
-  },
-  {
-    path: '/request-and-load/user/:userId/',
-    async: function ({ router, to, resolve }) {
-      // App instance
-      var app = router.app;
-
-      // Show Preloader
-      app.preloader.show();
-
-      // User ID from request
-      var userId = to.params.userId;
-
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
-            },
-            {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
-            },
-          ]
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            componentUrl: './pages/request-and-load.html',
-          },
-          {
-            props: {
-              user: user,
-            }
-          }
-        );
-      }, 1000);
+    path: '/tabs/',
+    componentUrl: './pages/tabs.html',
+    options: {
+      props: {
+        objectId: '2YAtnb0nxjjfbeZ3',
+      },
     },
+    tabs: [
+      {
+        id: 'tab1',
+        path: '/',
+        componentUrl: './pages/list.html',
+      },
+      {
+        id: 'tab2',
+        path: '/map/',
+        componentUrl: './pages/map.html',
+      },
+      {
+        id: 'tab3',
+        path: '/information/',
+        componentUrl: './pages/information.html',
+      },
+    ]
+  },
+  {
+    path: '/list/entries/:objectId',
+    componentUrl: './pages/detail.html',
   },
   // Default route (404 page). MUST BE THE LAST
   {
